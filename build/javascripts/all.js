262bc29ee75efb32b86dd7a7718218d0874b00ca
Luc Chaissac <hello@lucchaissac.com>
2016-11-12 20:48:48 +0100

    Mention Team Be ready

--- build/javascripts/all.js
+++ build/javascripts/all.js
@@ -1,1 +1,5 @@
-// This is where it all goes :)
+var list = document.querySelector('.shuffle');
+
+for (var i = list.children.length; i >= 0; i--) {
+  list.appendChild(list.children[Math.random() * i | 0]);
+}
